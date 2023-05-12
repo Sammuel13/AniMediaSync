b9d08447911943ad68894ef58056f83738fb0394
Sammuel13 <sammluna13@gmail.com>
2023-05-12 02:27:59 -0300

    changed library and did some planning

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pyautogui
+# import pyautogui
 import socket
 import threading
 
@@ -10,6 +10,27 @@ import threading
 
 # player = mpv.MPV()
 
+################################################################
+
+from pynput.keyboard import Key, Listener, Controller
+keyboard = Controller()
+
+# def on_press(key):
+#     print('{0} pressed'.format(key))
+
+
+# def on_release(key):
+#     print('{0} release'.format(key))
+#     if key == Key.esc:
+#         return False
+
+# with Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
+
+################################################################
+
+
+#TODO https://docs.python.org/3/library/configparser.html
 host = '127.0.0.1'
 port = 1234
 
@@ -28,14 +49,16 @@ def server_listener():
     while True:
         response = clientSocket.recv(2048)
         if response.decode('utf-8') == 'Playing!' or response.decode('utf-8') == 'Paused!':
-            pyautogui.press('playpause')
+            keyboard.press(Key.media_play_pause)
+        elif response.decode('utf-8') == 'Bye...': break
         print(response.decode('utf-8'))
         print('PLAY, EXIT: ')
 
 threading.Thread(target=server_listener).start()
 
 while True:
-    command = input()
+    command = input() #TODO change to key listener
     clientSocket.send(str.encode(command))
-    
+    if command.upper() == 'EXIT': break
+
 clientSocket.close()
